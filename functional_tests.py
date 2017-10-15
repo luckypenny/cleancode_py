31681adfea470fe10dc8cd09b7733d0fafb1195c
Joshua Kim <kyh79@inka.co.kr>
2017-10-15 19:39:21 +0900

    구글 브라우저로 변경

---
+++ functional_tests.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+import platform
+import time
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        # if platform.system() == "Windows":
+        #     # binary = FirefoxBinary('C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
+        #     binary = FirefoxBinary('C:\Dev\web_driver\geckodriver.exe')
+        # else:
+        #     binary = FirefoxBinary('/usr/bin/firefox')
+        #
+        # print("Platform : ", platform.system())
+        # self.browser = webdriver.Firefox(firefox_binary=binary)
+        # self.browser.implicitly_wait(3)
+
+        # # 크롬으로 변경
+        self.browser = webdriver.Chrome('C:\Dev\web_driver\chromedriver.exe')
+        time.sleep(5)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 웹 사이트를 확인하러 간다
+        self.browser.get('http://localhost:8000')
+        # self.browser.get('http://www.naver.com/')
+
+        # 웹 페이지 타이틀과 헤더가 ‘To-Do’를 표시하고 있다
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        #
+        # 그녀는 바로 작업을 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+        #
+        # "공작깃털 사기"라고 텍스트 상자에 입력한다
+        # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
+        inputbox.send_keys('공작깃털 사기')
+        #
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+        #
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        print("type : ", type(rows))
+        print("len(rows) : ", len(rows))
+        # self.assertTrue(
+        #     any(row.text == '1: 공작깃털 사기' for row in rows),
+        #     "신규 작업 아이템이 테이블에 표시되지 않는다"
+        # )
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
+        # 다시 "공작깃털을 사용해서 그물 만들기"라고 입력한다 (에디스는 매우 체계적인 사람이다)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 페이지는 다시 갱신되고, 2개 아이템이 목록에 보여진다.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows])
+
+        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
+        # 사이트는 그녀를 위한 특정 URL을 생성해 준다.
+        # 이때 URL에 대한 설명도 함께 제공된다.
+        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
+        self.fail('Finish the test!')
+        # 만족하고 잠자리에 든다.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,9 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
 
 from lists.views import home_page
-
 
 class HomePageTest(TestCase):
 
@@ -12,8 +11,21 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+        response = home_page(request)
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
