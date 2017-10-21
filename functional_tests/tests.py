317f662e72cb2261b205a5b772690198d16eba0b
Joshua Kim <kyh79@inka.co.kr>
2017-10-21 17:28:09 +0900

    functional_test 앱 생성 및 LiveServerTestCase 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,21 +6,11 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import platform
 import time
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
-        # if platform.system() == "Windows":
-        #     # binary = FirefoxBinary('C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
-        #     binary = FirefoxBinary('C:\Dev\web_driver\geckodriver.exe')
-        # else:
-        #     binary = FirefoxBinary('/usr/bin/firefox')
-        #
-        # print("Platform : ", platform.system())
-        # self.browser = webdriver.Firefox(firefox_binary=binary)
-        # self.browser.implicitly_wait(3)
-
-        # # 크롬으로 변경
         self.browser = webdriver.Chrome('C:\Dev\web_driver\chromedriver.exe')
         time.sleep(3)
 
@@ -35,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # self.browser.get('http://www.naver.com/')
 
         # 웹 페이지 타이틀과 헤더가 ‘To-Do’를 표시하고 있다
@@ -74,7 +64,3 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
         self.fail('Finish the test!')
         # 만족하고 잠자리에 든다.
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
